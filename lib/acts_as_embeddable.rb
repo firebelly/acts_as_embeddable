c8f712a9f1fb443845bd8dec68316385920ba7b0
keith <keith@firebellydesign.com>
2009-01-09 10:58:01 -0600

    first commit

---
+++ lib/acts_as_embeddable.rb
@@ -0,0 +1,36 @@
+require 'youtube'
+module Acts
+  module Embeddable
+  def self.included(base)
+    base.send :extend, ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_embeddable(field)
+      cattr_accessor :embeddable_text_field
+      self.embeddable_text_field = field.to_s
+        define_method "#{self.embeddable_text_field}" do
+          value = embed_embeddables(self[field])
+        end 
+      send :include, InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+  def embed_embeddables(text)
+    #video = text.match(/\[(youtube|vimeo)[width="\d*"\s?|height="\d*"\s?]*\](\w*)\[\/\1\]/)
+    test = /\[(youtube|vimeo)[width="\d*"\s?|height="\d*"\s?]*\](\S*v=|\s?)(\w*)\[\/\1\]/
+    video = text.match(test)
+    if video
+    case video[1]
+      when "youtube"    then Youtube.new(video[3], text).text
+      else text
+    end
+    end
+  end
+  
+  end
+end
+end
+
+ActiveRecord::Base.send :include, Acts::Embeddable
---
+++ lib/youtube.rb
@@ -0,0 +1,36 @@
+module Acts
+  module Embeddable
+    class Youtube
+      
+      def initialize(id, text)
+        test = /\[(youtube)[width="\d*"\s?|height="\d*"\s?]*\](\S*v=|\s?)(\w*)\[\/\1\]/
+        @id = id
+        @text = text
+        @text.gsub!(test, embed_code.gsub(/\{video\}/, @id).gsub(/\{width\}/, width_and_height(text)[:width]).gsub(/\{height}/, width_and_height(text)[:height]))
+      end
+      
+      def embed_code
+        embed = '<object width="{width}" height="{height}">'
+        embed += '<param name="movie" value="http://www.youtube.com/v/{video}"></param>'
+        embed += '<param name="allowFullScreen" value="true"></param>'
+        embed += '<param name="allowscriptaccess" value="always"></param>'
+        embed += '<embed src="http://www.youtube.com/v/{video}" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="{width}" height="{height}"></embed>'
+        embed += '</object>'
+      end
+      
+      def width_and_height(text)
+        test = /\[(youtube|vimeo)\s((width)=\\?"(\d*)\\?"\s?|(height)=\\?"(\d*)\\?"\s?)*/
+        dimensions = @text.match(test) || []
+        dimensions_obj = {}
+        dimensions_obj[:width] = dimensions[4] || 300.to_s
+        dimensions_obj[:height] = dimensions[6] || 243.to_s
+        dimensions_obj
+      end
+      
+      def text
+        @text
+      end
+      
+    end
+  end
+end
